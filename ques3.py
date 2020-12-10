a8647e4cf255b47f6f2eec85d92ac13ff7caaa92
Rabi098 <66278014+Rabi098@users.noreply.github.com>
2020-12-10 20:20:53 +0500

    Update ques3.py

--- ques3.py
+++ ques3.py
@@ -1,5 +1,5 @@
 import time
-lyrics="Before You Go#Lewis Capaldi#I fell by the wayside, like everyone else#I hate you, I hate you, I hate you#But I was just kidding myself#Our every moment, I start to replace#Cause now that they re gone#All I hear are the words that #I needed to say"
+lyrics="You were the popular one, the popular chick,#Standing on the field with your pretty pompons#Now you're working at the movie selling popular corn#I could have been a mess but I never went wrong#'Cause I'm putting down my story in a popular song"
 sentence=lyrics.split('#')
 l=len(sentence)
 for i in range(l):
